45ae8006f323393510eba8d5b52588a9cd314098
victorcardosoferraz-collab <victor.cardoso.ferraz@escola.pr.gov.br>
2026-09-16 13:43:40 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const botoesCurtir = document.querySelectorAll(".curtir");
 
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+
+    botaoCurtir.addEventListener("click", curtir);
+
+    function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;
+        }
+        else{
+            contador.textContent--;
+            curtiu = false;
+        }
+    }
+});
